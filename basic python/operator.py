f44c52534b926c26a895e8f34d65c73a1d426d48
Suraj Singh Rana <147970666+surajrana44@users.noreply.github.com>
2026-03-30 14:03:01 +0530

    snippets of dict,operator,varia_storage

---
+++ basic python/operator.py
@@ -0,0 +1,16 @@
+ 
+a = 2
+b = 3
+print (a+b) # -,*,%,/,// they are are used
+
+a = a+2 # method 1
+a +=2 # method 2
+
+x,y = 6,5 # assign two value 
+print (x<y)
+print ( x <= b)
+print ( x >= b)
+print (x == b)
+print (x!= y)
+print ( x<10 and y>=5 )
+print (x<10 or y>10)
